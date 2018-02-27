cd6c0935a1f0c96b3c3ae32053e795445bd91392
Jack Williams <jack.williams497@gmail.com>
2018-02-27 17:00:59 +0400

    Version 0.1.1.5
    - Added Basic player movement using a RigidBody and providing velocity

---
+++ Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	private Rigidbody2D myRigidBody;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		myRigidBody.velocity = direction * moveSpeed * Time.deltaTime;
+
+			
+	}
+}
