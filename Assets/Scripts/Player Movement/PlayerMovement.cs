55ad3f61508bf3c650def3daf6eb624ab8117fac
Jack Williams <jack.williams497@gmail.com>
2018-03-12 15:40:07 +0400

    Fixed Movement Animations
    Player now correctly faces the direction they are moving in on the x axis

--- Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Rigidbody2D myRigidBody;
 	public float moveSpeed;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () 
 	{
-		myRigidBody = GetComponent<Rigidbody2D>();	
+		myRigidBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();	
 	}
 	
 	// Update is called once per frame
@@ -23,6 +25,7 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate()
 	{
 		Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		anim.SetFloat("x", (int)direction.x);
 
 		myRigidBody.velocity = direction * moveSpeed * Time.deltaTime;
 
